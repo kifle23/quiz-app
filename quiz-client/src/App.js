9611290c4c81085edc6fc91c11114f58e1f8840d
kifle23 <kiflekifle23@gmail.com>
2023-07-05 12:28:32 +0300

    Protected Routes

--- quiz-client/src/App.js
+++ quiz-client/src/App.js
@@ -4,15 +4,18 @@ import Login from './components/Login';
 import Quiz from './components/Quiz';
 import Result from './components/Result';
 import Layout from './components/Layout';
+import Authenticate from './components/Authenticate';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Login />} />
-        <Route path="/" element={<Layout />}>
-          <Route path='/quiz' element={<Quiz />} />
-          <Route path='/result' element={<Result />} />
+        <Route element={<Authenticate />}>
+          <Route path="/" element={<Layout />}>
+            <Route path="/quiz" element={<Quiz />} />
+            <Route path="/result" element={<Result />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
